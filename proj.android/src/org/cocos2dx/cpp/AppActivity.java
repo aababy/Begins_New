96a4f01994289ec79f0edadcfd3ebbfdd8aa1bb4
aababy <jumpchess@163.com>
2014-10-25 15:21:20 +0800

    ui

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -27,6 +27,14 @@ THE SOFTWARE.
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 public class AppActivity extends Cocos2dxActivity {
+	public Cocos2dxGLSurfaceView onCreateView() {  
+	       Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);  
+	       // TestCpp should create stencil buffer  
+	       glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);  
+	          
+	       return glSurfaceView;
+	}
 }
